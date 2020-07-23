a0365c0713f71f0358ae177c6f9b4c9599485a1e
mobiletechtracker <info@mobiletechtracker.co.uk>
2020-07-23 18:23:14 +0100

    Add the first milestone.

---
+++ Milestone-01/AudioPlayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace AudioPlayer
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Please provide device identifier.");
+            var identifier = Console.ReadLine();
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:57100/devicesHub")
+                .Build();
+
+            await connection.StartAsync();
+            await connection.InvokeAsync("ReceiveDeviceConnected", identifier);
+
+            while (true)
+            {
+                await connection.InvokeAsync("ReceiveHeartbeat", identifier);
+                await Task.Delay(30000);
+            }
+        }
+    }
+}
---
+++ Milestone-01/PlaneScheduleManager/Data/UserMappings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PlainScheduleController.Data
+{
+    public static class UserMappings
+    {
+        private static Dictionary<string, string> connections = new Dictionary<string, string>();
+
+        public static void AddDeviceConnected(string deviceId, string connectionId)
+        {
+            connections[connectionId] = deviceId;
+        }
+
+        public static void RemoveDeviceConnected(string connectionId)
+        {
+            connections.Remove(connectionId);
+        }
+
+        public static string GetDeviceId(string connectionId)
+        {
+            if (!connections.ContainsKey(connectionId))
+                return string.Empty;
+
+            return connections[connectionId];
+        }
+    }
+}
